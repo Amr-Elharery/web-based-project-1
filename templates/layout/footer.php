c7257217f5d77d4bb5a8a874d4acdffb77974bac
Mohamed_mahmoud <119744393+Mohamedmshawky@users.noreply.github.com>
2025-03-28 05:38:33 +0200

    Create footer.php

---
+++ templates/layout/footer.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Footer</title>
+        <link rel="stylesheet" href="/Shawky/css/global.css">
+        <link rel="stylesheet" href="/Shawky/css/footer.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+</head>
+    <body>
+        <footer>
+            <div class="container justify-center items-center">
+                <p>&copy; <?php echo date("Y"); ?> Your Website. All Rights Reserved.</p>
+                
+                <ul class="footer-links">
+                    <li><a href="Shawky/about.php">About Us</a></li>
+                    <li><a href="Shawky/services.php">Services</a></li>
+                    <li><a href="Shawky/contact.php">Contact</a></li>
+                </ul>
+
+                <div class="social-icons">
+                    <a href="https://facebook.com" target="_blank"><i class="fa-brands fa-facebook"></i></a>
+                    <a href="https://twitter.com" target="_blank"><i class="fa-brands fa-twitter"></i></a>
+                    <a href="https://instagram.com" target="_blank"><i class="fa-brands fa-instagram"></i></a>
+                </div>
+            </div>
+        </footer>
+
+        
+    </body>
+</html>
