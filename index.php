70de2ccbdfafce7d732222d417b62cb82a381ad2
Amr Tamer Elharery <amrelharery@gmail.com>
2025-03-29 19:06:24 +0200

    Add Signup content

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./public/css/global.css">
+  <link rel="stylesheet" href="./public/css/header.css">
+  <link rel="stylesheet" href="./public/css/sign-up.css">
+  <link rel="stylesheet" href="./public/css/footer.css">
+  <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+  <title>Signup</title>
+</head>
+<body>
+  <?php include __DIR__ . '/templates/layout/header.php'; ?>
+  <?php include __DIR__ . '/templates/sign-up.php'; ?>
+  <?php include __DIR__ . '/templates/layout/footer.php'; ?>
+
+  <script src="./public/js/header.js"></script>
+</body>
+</html>
--- templates/layout/header.php
+++ templates/layout/header.php
@@ -1,43 +1,24 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Navigation Bar</title>
-        <link rel="stylesheet" href="../../public/css/global.css">
-        <link rel="stylesheet" href="../../public/css/header.css">
-        <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
-    </head>
-    <body>
-        <nav>
-            <div class="container justify-center items-center">
-                <a class="logo" href="#"><i class="fa-solid fa-a fa-2xl"></i></a>
+<header>
+<nav>
+    <div class="container flex flex-between items-center">
+        <a class="logo" href="#"><i class="fa-solid fa-a fa-2xl"></i></a>
 
-                <button class="nav-toggle" onclick="toggleNav()">Menu</button>
+        <button class="btn btn-effect nav-toggle" onclick="toggleNav()">Menu</button>
 
-                <ul class="nav-list-ul">
-                    <li class="close-btn">
-                        <button onclick="toggleNav()">Close</button>
-                    </li>
-                    <li class="nav p-10 nav-link">
-                        <a href="#">Home</a>
-                    </li>
-                    <li class="nav p-10 nav-link">
-                        <a href="#">-----</a>
-                    </li>
-                    <li class="nav p-10 nav-link">
-                        <a href="#">-----</a>
-                    </li>
-                    <li class="nav p-10 nav-link">
-                        <a href="#">------</a>
-                    </li>
-                    <li class="nav p-10 nav-link">
-                        <a href="#">------</a>
-                    </li>
-                </ul>
-            </div>
-        </nav>
-
-        <script src="../../public/js/header.js"></script>
-    </body>
-</html>
+        <ul class="nav-list-ul">
+            <li class="close-btn">
+                <button onclick="toggleNav()">Close</button>
+            </li>
+            <li class="nav p-10 nav-link">
+                <a href="/">Home</a>
+            </li>
+            <li class="nav p-10 nav-link">
+                <a href="#">Login</a>
+            </li>
+            <li class="nav p-10 nav-link">
+                <a href="/">Sign up</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+</header>
