eb912467ee6166dea8ae667bbef370f72b5a4096
Amr Tamer Elharery <amrelharery@gmail.com>
2025-04-01 23:56:16 +0200

    Fix header colors

--- templates/layout/header.php
+++ templates/layout/header.php
@@ -9,7 +9,9 @@
 
         <ul class="nav-list-ul">
             <li class="close-btn">
-                <button onclick="toggleNav()">Close</button>
+                <button class="btn btn-effect c-white" onclick="toggleNav()">
+                    <i class="fa-solid fa-xmark"></i>
+                </button>
             </li>
             <li class="nav p-10 nav-link">
                 <a href="/">Home</a>
